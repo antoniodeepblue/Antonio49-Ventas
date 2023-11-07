ad2808465c363c439ccc658090db8b4aa78862c8
Antonio Fernandez <Antonio49@users.noreply.huggingface.co>
2023-11-07 10:47:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ interfaz = gr.Interface(
     outputs=gr.Textbox(label='Kmeans Grupo:'),
     title='Ventas de productos. K-means',
     description='Este modelo está desarrollado para la agrupacion Kmeans de productos.',
-    article= "Autor: <a href=\"https://huggingface.co/Antonio49\">Antonio Fernández</a>. de <a href=\"https://saturdays.ai/\">SaturdaysAI</a>. Aplicación desarrollada con fines docentes',
+    article= 'Autor: <a href=\"https://huggingface.co/Antonio49\>Antonio Fernández</a>. de <a href=\"https://saturdays.ai/\">SaturdaysAI</a>. Aplicación desarrollada con fines docentes',
     theme='peach'
     examples = [[0,'Fresk', 0,'Milk', 0,'Grocery', 0,'Frozen', 0,'Detergents_Paper', 0, 'Delicassen', 0,'Channel1',0,'Channel2',0,'Region1',0,'Region2',0,'Region3'],
             [0,'Fresk', 1,'Milk', 2,'Grocery', 4,'Frozen', 1,'Detergents_Paper', 0, 'Delicassen', 0,'Channel1',0,'Channel2',0,'Region1',0,'Region2',0,'Region3']]
